cced89dda09bbc829a8426d3181c14c07fc9936b
shoyo-inokuchi <shoyoinokuchi@gmail.com>
2019-10-21 16:11:23 +0900

    Refactor courses schema and remigrate

--- course_rater_app/models.py
+++ course_rater_app/models.py
@@ -7,20 +7,51 @@ RATING_CHOICES = [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=100)
-    professor = models.CharField(max_length=100)
+    title = models.CharField(max_length=300)
+    instructor = models.CharField(max_length=300)
+    credits = models.IntegerField()
+    level = models.CharField(max_length=200) # Ex. "Final stage advanced-level undergraduate"
+    category = models.CharField(max_length=200) # Ex. "Elective Subjects"
+
+    school = models.CharField(max_length=200)
+    campus = models.CharField(max_length=200)
+
+    main_language = models.CharField(max_length=100)
+    eligible_year = models.CharField(max_length=100) # Ex. "4th year and above"
+    course_code = models.CharField(max_length=50)
+    course_class_code = models.CharField(max_length=50)
+
+    syllabus_url = models.URLField()
+
+    first_academic_discipline = models.CharField(max_length=200)
+    second_academic_discipline = models.CharField(max_length=200)
+    third_academic_discipline = models.CharField(max_length=200)
+
+    
+    # Can change year-to-year
+    classroom = models.CharField(max_length=200)
+    year = models.CharField(max_length=10)
+    # TODO: parse into following
+    term_day_period = models.CharField(max_length=200)
+    term = models.CharField(max_length=100, null=True, blank=True)
+    day = models.CharField(max_length=100, null=True, blank=True)
+    period = models.CharField(max_length=100, null=True, blank=True)
+
 
 
 class CourseReview(models.Model):
+    # Relationships
     course = models.ForeignKey('Course', on_delete=models.CASCADE)
     reviewer = models.ForeignKey(settings.AUTH_USER_MODEL,
                                  db_column='user_id',
                                  on_delete=models.CASCADE)
 
+    # User reviews
     overall_rating = models.IntegerField(choices=RATING_CHOICES)
     text = models.CharField(max_length=5000)
-
     anonymous = models.BooleanField(default=False)
+
+    # Metadata
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_updated = models.DateTimeField(auto_now=True)
 
